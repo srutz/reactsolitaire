7ce6e474d3ad51c155a78f9788c00ccdee19bbcf
stepan.rutz <stepan.rutz@gmx.de>
2024-09-17 22:16:45 +0200

    large text

--- src/components/Menubar.tsx
+++ src/components/Menubar.tsx
@@ -12,10 +12,10 @@ export function Menubar({handleClick} : { handleClick: (action: string) => void 
                 <div className="text-white text-3xl font-bold select-none uppercase tracking-tighter"><span className="text-green-400">React</span> Solitaire</div>
                 <div className="grow"></div>
                 <nav className="flex gap-4">
-                <a href="#" className="text-gray-300 hover:text-white" onClick={() => { handleClick("game-new")}}>New game</a>
-                <a href="#" className="text-gray-300 hover:text-white" onClick={() => { handleClick("game-stop")}}>Stop game</a>
-                <a href="#" className="text-gray-300 hover:text-white" onClick={() => { handleClick("about")}}>About</a>
-                <a href="#" className="text-gray-300 hover:text-white" onClick={() => { handleClick("share")}}><img alt="Share" src="/reactsolitaire/images/share.svg" /></a>
+                <a href="#" className="text-gray-300 hover:text-white text-lg tracking-tight" onClick={() => { handleClick("game-new")}}>New game</a>
+                <a href="#" className="text-gray-300 hover:text-white text-lg tracking-tight" onClick={() => { handleClick("game-stop")}}>Stop game</a>
+                <a href="#" className="text-gray-300 hover:text-white text-lg tracking-tight" onClick={() => { handleClick("about")}}>About</a>
+                <a href="#" className="text-gray-300 hover:text-white text-lg tracking-tight" onClick={() => { handleClick("share")}}><img alt="Share" src="/reactsolitaire/images/share.svg" /></a>
                 </nav>
             </div>
         </div>
